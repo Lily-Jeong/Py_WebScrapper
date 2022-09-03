4f43b52124e924fa222bdb03f7694b3d78df2962
Lily Jeong <109122104+Lily-Jeong@users.noreply.github.com>
2022-09-03 09:16:06 +0000

    scraping from different website

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 from requests import get
-import selenium
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+browser = webdriver.Chrome(options=options)
+
+browser.get("https://kr.indeed.com/jobs?q=python&l&vjk=9f50e72b1130520d")
+
+soup = BeautifulSoup(browser.page_source, "html.parser")
+job_list = soup.find("ul", class_="jobsearch-ResultsList css-0")
+jobs = job_list.find_all("li")
+for job in jobs:
+  zone = job.find("div", class_="mosaic-zone")
+  if zone == None:
+    anchor = job.select("h2 a")
+    print(anchor)
+    print("///////////")
+    
