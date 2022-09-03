a4a9893432f3b43cd12d320ad725726ff60de773
Lily Jeong <109122104+Lily-Jeong@users.noreply.github.com>
2022-09-03 10:32:08 +0000

    File "main.py", line 24, in <module>     title = anchor['aria-label'] TypeError: list indices must be integers or slices, not str

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from requests import get
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -9,15 +8,29 @@ options.add_argument("--disable-dev-shm-usage")
 
 browser = webdriver.Chrome(options=options)
 
-browser.get("https://kr.indeed.com/jobs?q=python&l&vjk=9f50e72b1130520d")
+base_url = 'https://kr.indeed.com/jobs?q='
+keyword = 'python'
 
+browser.get(f"{base_url}{keyword}")
+
+results = []
 soup = BeautifulSoup(browser.page_source, "html.parser")
 job_list = soup.find("ul", class_="jobsearch-ResultsList css-0")
-jobs = job_list.find_all("li")
+jobs = job_list.find_all('li')
 for job in jobs:
   zone = job.find("div", class_="mosaic-zone")
   if zone == None:
     anchor = job.select("h2 a")
-    print(anchor)
-    print("///////////")
-    
+    title = anchor['aria-label']
+    link = anchor['href']
+    company = job.find("span", class_="companyName")
+    location = job.find("div", class_="companyLocation")
+    job_data = {
+      'link': f"https://kr.indeed.com/{link}",
+      'company': company.string,
+      'location': location.string,
+      'position': title
+    }
+    results.append(job_data)
+for result in results:
+  print(result, "\n//////////\n")
