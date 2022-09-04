153e1c9292de885b0c149f75f9b40c8ac1b9c28b
Lily Jeong <109122104+Lily-Jeong@users.noreply.github.com>
2022-09-04 08:20:54 +0000

    fixing TypeError

--- main.py
+++ main.py
@@ -18,21 +18,21 @@ soup = BeautifulSoup(browser.page_source, "html.parser")
 job_list = soup.find("ul", class_="jobsearch-ResultsList css-0")
 jobs = job_list.find_all('li', recursive=False)
 for job in jobs:
-  zone = job.find("div", class_="mosaic-zone")
-  if zone == None:
-    anchor = job.select("h2 a")
-    print(anchor)
-    """
-    title = anchor['aria-label']
-    link = anchor['href']
-    company = job.find("span", class_="companyName")
-    location = job.find("div", class_="companyLocation")
-    job_data = {
-      'link': f"https://kr.indeed.com/{link}",
-      'company': company.string,
-      'location': location.string,
-      'position': title
-    }
+    zone = job.find("div", class_="mosaic-zone")
+    if zone == None:
+        anchor = job.select("h2 a")
+        print(anchor)
+        """
+        title = anchor['aria-label']
+        link = anchor['href']
+        company = job.find("span", class_="companyName")
+        location = job.find("div", class_="companyLocation")
+        job_data = {
+          'link': f"https://kr.indeed.com/{link}",
+          'company': company.string,
+          'location': location.string,
+          'position': title
+        }
     results.append(job_data)
 for result in results:
   print(result, "\n//////////\n")
