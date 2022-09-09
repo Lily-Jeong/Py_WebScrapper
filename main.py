ab4601eb7faf7f4380a430c32fe7c4cfd80043f2
Lily Jeong <lily.jeong0628@gmail.com>
2022-09-09 18:42:31 +0900

    #6 still in progress...

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+#from extractors.indeed import extract_indeed_jobs
+from extractors.planet import extract_planet_jobs
+from extractors.wwr import extract_wwr_jobs
 
 app = Flask("JobScrapper")
 
@@ -7,7 +10,12 @@ def home():
     return render_template("home.html")
 
 @app.route("/search")
-def hello():
-    return render_template("search.html")
+def search():
+    keyword = request.args.get("keyword")
+    #indeed = extract_indeed_jobs(keyword)
+    planet = extract_planet_jobs(keyword)
+    wwr = extract_wwr_jobs(keyword)
+    jobs = planet + wwr #+ indeed
+    return render_template("search.html", keyword=keyword, jobs=jobs)
 
 app.run("127.0.0.1")
