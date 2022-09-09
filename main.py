12969642248bbbeb37c031b0da64be3e5b3bfe17
Lily Jeong <lily.jeong0628@gmail.com>
2022-09-09 18:08:56 +0900

    #6 still in progress...

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ app = Flask("JobScrapper")
 def home():
     return render_template("home.html")
 
-@app.route("/hello")
+@app.route("/search")
 def hello():
-    return 'hello you!'
+    return render_template("search.html")
 
 app.run("127.0.0.1")
